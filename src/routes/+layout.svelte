6bb6595640d3bce4b81362da43c59b30713ed44f
Cho JeongTaek <cjt3591@gmail.com>
2023-09-13 18:34:25 +0900

    📦️ Chore :  개발 환경 셋팅

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import './styles.css';
+</script>
+
+<div class="app">
+	네비게이션
+	<slot />
+</div>
+
+<style>
+	
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div>
+	메인
+</div>
+
+<style>
+	
+</style>
